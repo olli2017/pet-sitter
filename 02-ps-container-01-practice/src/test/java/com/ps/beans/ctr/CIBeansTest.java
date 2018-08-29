551e558b187e9b023d260c7e59d770b0b4ef69af
Aydar <dusmukhambetov@mail.ru>
2018-08-29 14:58:22 +0300

    Delete //TODO 3, because task complete

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
@@ -24,7 +24,6 @@ public class CIBeansTest {
             logger.info("Bean " + beanName + " of type " + ctx.getBean(beanName).getClass().getSimpleName());
         }
 
-        //TODO 3. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
         ComplexBeanImpl complexBean0 = (ComplexBeanImpl) ctx.getBean("complexBean0");
         ComplexBeanImpl complexBean1 = (ComplexBeanImpl) ctx.getBean("complexBean1");
         ComplexBean2Impl complexBean2 = (ComplexBean2Impl) ctx.getBean("complexBean2");
